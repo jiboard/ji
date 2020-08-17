217bab6feca2b3d3c031e592ece23deb1ec89134
zhongl <zhong.lunfu@gmail.com>
2020-08-17 13:39:04 +0800

    Upgrade copyright and fix version.

--- loader/src/main/java/ji/loader/Main.java
+++ loader/src/main/java/ji/loader/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Zhong Lunfu
+ * Copyright 2020 Zhong Lunfu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
